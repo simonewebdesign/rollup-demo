095b3c3d7470efa85f8069af84813db25f30ac71
Simone Vittori <hello@simonewebdesign.it>
2015-10-26 11:07:06 +0000

    Initial commit

---
+++ source/baz.js
@@ -0,0 +1,6 @@
+// baz.js
+export default function () {
+  let result = 5;
+  console.log('baz!');
+  return result;
+}
---
+++ source/main.js
@@ -0,0 +1,7 @@
+import './foo';
+import './bar';
+import baz from './baz';
+
+baz();
+
+console.log('main done.');
