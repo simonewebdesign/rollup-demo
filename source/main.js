064b2d1cb415566a47fccc302a94ff940d344ed5
Simone Vittori <hello@simonewebdesign.it>
2015-10-26 16:56:48 +0000

    React

--- source/main.js
+++ source/main.js
@@ -1,7 +1,22 @@
 import './foo';
 import './bar';
-import baz from './baz';
+import baz from './baz'; // default import
+
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 baz();
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return React.createElement('div', { className: 'app-content' });
+    // return <div>Hello World</div>;
+  }
+}
+
+ReactDOM.render(React.createElement(App, null), document.getElementById('app'));
 
 console.log('main done.');
